a6ebe0f76a33aa0c976f54a1ca5d6b88a2ef81b5
niko6623 <niko6623@DESKTOP-HD7FAL2>
2016-09-12 12:17:35 +0200

    Add project files.

---
+++ 03_Simon_Says/Simon.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _03_Simon_Says
+{
+    internal class Simon
+    {
+        internal string Echo(string v)
+        {
+            return v.ToLower();
+        }
+
+        internal string Shout(string v)
+        {
+            return v.ToUpper();
+        }
+
+        internal string Repeat(string v)
+        {
+            return v + " " + v;
+        }
+
+        internal object Repeat(string v1, int v2)
+        {
+            string All = "";
+            for (int i = 0; i < v2; i++)
+            {
+                All = All+ " " + v1;
+            }
+            return All.Trim();
+        }
+    }
+}
+
+  
+
+
+
+
+
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _03_Simon_Says;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
